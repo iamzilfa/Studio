652913b96849b72f12d509fad1c11c58c4aab33a
Zilfa Cyamani <zilcyam@gmail.com>
2020-12-22 21:31:35 +0200

    editing gallery/urls.py

--- gallery/urls.py
+++ gallery/urls.py
@@ -11,5 +11,6 @@ urlpatterns=[
     url(r'^location/(?P<location>\w+)',views.image_location,name ='location')
 ]
 
+
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
